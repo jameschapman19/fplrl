b171325a6ddad3ee5910b16f75ee99e900f2f9b6
James Chapman <james.chapman.19@ucl.ac.uk>
2021-08-30 16:52:25 +0100

    starting 11 selectors and squad selectors

--- fplrl/utils.py
+++ fplrl/utils.py
@@ -1,5 +1,4 @@
-import pandas as pd
-from fplrl.data_utils.collector import merge_all_gws, merge_gw
+from fplrl.data_utils.collector import merge_gw
 from fplrl.data_utils.global_merger import merge_data
 
 
@@ -7,11 +6,3 @@ def update_data():
     # merge_all_gws(3, 'C:/Users/chapm/PycharmProjects/fplrl/data/2021-22/gws')
     merge_gw(3, 'C:/Users/chapm/PycharmProjects/fplrl/data/2021-22/gws')
     merge_data()
-
-
-def main():
-    update_data()
-
-
-if __name__ == '__main__':
-    main()
